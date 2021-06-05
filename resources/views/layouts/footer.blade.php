2a0403b3a7d744478ce5fecd402c6075dbcfcf6f
danh <0974619741danh@gmail.com>
2021-06-05 19:57:49 +0700

    Laravel

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,12 @@
+<!-- Footer -->
+<footer class=" mt-auto bg-dark text-center text-lg-start px-0">
+  <!-- Grid container -->
+  <!-- Copyright -->
+  <div class="text-center p-3 text-muted" >
+    © 2021 Copyright:
+    <a class="text-secondary" href="home">EVN Khánh Hòa.com</a>
+    | Designed by <strong>Duy Danh</strong>
+  </div>
+  <!-- Copyright -->
+</footer>
+<!-- Footer -->
---
+++ resources/views/layouts/jumbotron.blade.php
@@ -0,0 +1,7 @@
+<div class="header" style="margin-bottom:0">
+
+        <a href=""><img src="{{asset('img/logo.png')}}" alt="" class="logo__img"></a>
+
+
+</div>
+</div>
