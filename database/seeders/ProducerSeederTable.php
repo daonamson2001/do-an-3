7f102b7ad9955eea87ef983bfd6bcafb02352648
danh <0974619741danh@gmail.com>
2021-06-12 22:03:25 +0700

    laravel

--- database/seeders/ProducerSeederTable.php
+++ database/seeders/ProducerSeederTable.php
@@ -14,18 +14,18 @@ class ProducerSeederTable extends Seeder
     public function run()
     {
         //
-        $faker = \Faker\Factory::create();
-        for ($i=1; $i < 200; $i++) { 
-            # code...       
-             DB::table('producers')->insert([
-                'pro_name'=>$faker->name,
-                'pro_address'=>$faker->country,
-                'pro_phone'=> '0'.$faker->numberBetween($min = 100000000, $max = 999999999),
-                'pro_email'=> $faker->email,
-                'pro_employee'=> $faker->name,
-                'dis_id'=>$faker->numberBetween($min = 1, $max = 7),
-            ]);
-        }
+        // $faker = \Faker\Factory::create();
+        // for ($i=1; $i < 200; $i++) { 
+        //     # code...       
+        //      DB::table('producers')->insert([
+        //         'pro_name'=>$faker->name,
+        //         'pro_address'=>$faker->country,
+        //         'pro_phone'=> '0'.$faker->numberBetween($min = 100000000, $max = 999999999),
+        //         'pro_email'=> $faker->email,
+        //         'pro_employee'=> $faker->name,
+        //         'dis_id'=>$faker->numberBetween($min = 1, $max = 7),
+        //     ]);
+        // }
     }
 }
 
